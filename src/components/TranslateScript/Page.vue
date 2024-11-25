35f3300e918f1562f7cfe660df6a3903a6d1b6b9
XBB <icexb_baka@163.com>
2024-11-26 01:02:18 +0800

    fix: tsc error

--- src/components/TranslateScript/Page.vue
+++ src/components/TranslateScript/Page.vue
@@ -32,7 +32,7 @@ import {specialCharOption, UploadOption} from "../../models/constants.ts";
 import {definedEvent, emitter} from "../../event/emitter.ts";
 import {IScript} from "../../models/scripts/model.ts";
 import storageManager, {StorageKey} from "../../storage";
-import router, {getRoute, homeRoute} from "../../router";
+import router, {homeRoute} from "../../router";
 import ConfirmOverride from "../General/ConfirmOverride.vue";
 
 const updateItem = (index: number, updatedItem: ITranslateItem) => {
